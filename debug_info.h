48c172c24460649408e449c7eeb806729d9b54f6
MrTobikun <ally.manager@protonmail.com>
2020-01-20 01:36:04 +0300

    Header added

---
+++ debug_info.h
@@ -0,0 +1,26 @@
+#ifndef DEBUG_INFO_H
+#define DEBUG_INFO_H
+
+#include <cstdio>
+#include <cstring>
+#define NAME_LOG "debug.txt"
+
+static bool isOpened = false;
+
+static void clear_log()
+{
+  std::remove(NAME_LOG);
+}
+
+static void debug_to_file()
+{
+  if (!isOpened) {
+    freopen(NAME_LOG, "w", stderr);
+    isOpened = true;
+  }
+}
+
+#define DEBUG_TO_FILE_FORMAT(fmt, ...) debug_to_file(); fprintf(stderr, "[%s] ", __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__);
+#define DEBUG_TO_FILE(fmt, ...) debug_to_file(); fprintf(stderr, "[%s] %s", __FUNCTION__, fmt, ##__VA_ARGS__);
+
+#endif // DEBUG_INFO_H
