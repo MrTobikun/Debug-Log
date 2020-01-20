1be2c3d13ebcf18e17e52d502257b267e408b3e3
MrTobikun <58442983+MrTobikun@users.noreply.github.com>
2020-01-20 22:20:05 +0300

    Added close descriptor

--- debug_info.h
+++ debug_info.h
@@ -20,7 +20,12 @@ static void debug_to_file()
   }
 }
 
-#define DEBUG_TO_FILE_FORMAT(fmt, ...) debug_to_file(); fprintf(stderr, "[%s] ", __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__);
-#define DEBUG_TO_FILE(fmt, ...) debug_to_file(); fprintf(stderr, "[%s] %s", __FUNCTION__, fmt, ##__VA_ARGS__);
+static void close_descriptor()
+{
+  fclose(stderr); 
+}
+
+#define DEBUG_TO_FILE_FORMAT(fmt, ...) debug_to_file(); fprintf(stderr, "[%s] ", __FUNCTION__); fprintf(stderr, fmt, ##__VA_ARGS__); close_descriptor();
+#define DEBUG_TO_FILE(fmt, ...) debug_to_file(); fprintf(stderr, "[%s] %s", __FUNCTION__, fmt, ##__VA_ARGS__); close_descriptor();
 
 #endif // DEBUG_INFO_H
